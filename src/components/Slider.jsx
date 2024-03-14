049c009442e2c94dd1e87df6b3e3116b5081e589
NabinKhadka1 <nabinkhadka330@gmail.com>
2024-03-15 00:03:51 +0545

    Carousel addons

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,10 +4,14 @@ import '../App.css';
 const Slider = ({ movies }) => {
   const imgPath = import.meta.env.VITE_IMG_PATH;
   return (
-    <Carousel>
+    <Carousel className="pt-2">
       {movies.map((movie) => (
-        <div key={movie.id}   className="carousel-item">
+        <div key={movie.id} className="h-[500px]">
           <img className="w-full h-full object-cover" src={`${imgPath}${movie.poster_path}`} alt={movie.title}/>
+          <div className="legend">
+            <span className="text-4xl font-semibold text-white">{movie.title}</span>
+            <p className="mt-2">{movie.overview}</p>
+          </div>
         </div>
       ))}
     </Carousel>
